12e736739a84e65599cf90e711fccf00f9df7760
Adam_lewis <adam.lewis@egm.io>
2026-07-15 10:51:05 +0200

    remove: email extension

--- src/ckanext-custom-emails/ckanext/custom_emails/plugin.py
+++
@@ -1,8 +0,0 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
-
-class CustomEmailsPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-
-    def update_config(self, config):
-        toolkit.add_template_directory(config, 'templates')
